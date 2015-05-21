2f64389afe74a889198a7835fa6438dc6b21f529
Matthew Duffin <matthewjduffin@gmail.com>
2015-05-22 00:15:59 +0100

    Now using a struct to store the matrix

--- inverter.c
+++ inverter.c
@@ -1,19 +1,42 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
-double *readMatrix(char *fileName, int *size);
-void assignValues(double *matrix, char *buffer, int line_number, int size);
-void insertMat(double *matrix, int size, int col, int row, double x);
-void initial(double *matrix, int size, int col, int row, double x);
-double accessMat(double *matrix, int size, int col, int row);
-void printMat(double *matrix, int size, int start);
-void swapRow(double *matrix, int size, int ra, int rb);
-int pivMax(double *matrix, int size, int k);
-void backSub(double *matrix, int size);
-void inverse(double *matrix, int size);
+typedef struct Matrix {
+    int size;
+    double *data;
+} Matrix;
 
-double *readMatrix(char *fileName, int *size) {
+Matrix *readMatrix(char *fileName);
+void assignValues(Matrix *mat, char *buffer, int line_number);
+void insertMat(Matrix *mat, int col, int row, double x);
+void initial(Matrix *mat, int col, int row, double x);
+double accessMat(Matrix *mat, int col, int row);
+void printMat(Matrix *mat, FILE *fp, int start);
+void swapRow(Matrix *mat, int ra, int rb);
+int pivMax(Matrix *mat, int k);
+void backSub(Matrix *mat);
+void inverse(Matrix *mat);
+
+Matrix *createMat(int size) {
+    Matrix *mat = malloc(sizeof(Matrix));
+    assert(mat != NULL);
+    
+    mat -> size = size;
+    mat -> data = malloc(size * size * 2 * sizeof(double));
+    return mat;
+}
+
+void destroyMat(Matrix *mat) {
+    assert(mat != NULL);
+    
+    free(mat -> data);
+    free(mat);
+}
+
+Matrix *readMatrix(char *fileName) {
     FILE *fp = fopen(fileName, "r");
     
     if (fp == NULL) {
@@ -21,20 +44,21 @@ double *readMatrix(char *fileName, int *size) {
         return NULL;
     }
     int ch;
-    while (EOF!=(ch=fgetc(fp)) && ch != '\n') {
-        if (ch == ' ') (*size)++;
+    int size = 0;
+    while (EOF != (ch = fgetc(fp)) && ch != '\n') {
+        if (ch == ' ') size++;
     }
-    (*size)++;
+    size++;
     rewind(fp);
     
     size_t buffer_size = 80;
     char *buffer = malloc(buffer_size * sizeof(char));
     
-    double *matrix = malloc((*size) * (*size) * sizeof(double) * 2);
+    Matrix *mat = createMat(size);
     //read each line and write to matrix.
     int line_number = 0;
     while(-1 != getline(&buffer, &buffer_size, fp)) {
-        assignValues(matrix, buffer, line_number, *size);
+        assignValues(mat, buffer, line_number);
         line_number++;
     }
     fflush(stdout);
@@ -43,64 +67,69 @@ double *readMatrix(char *fileName, int *size) {
     fclose(fp);
     free(buffer);
     
-    return matrix;
+    return mat;
 }
 
-void assignValues(double *matrix, char *buffer, int line_number, int size) {
-    int count = 0;
+void writeMatrix(Matrix *mat) {
+    FILE *fp;
+    fp = fopen("output.txt","w+");
+    printMat(mat, fp, mat -> size);
+    fclose(fp);
+}
+void assignValues(Matrix *mat, char *buffer, int line_number) {
+    int count;
     char *token;
     double val;
-    while (count < size) {
+    for(count = 0; count < mat -> size; count++) {
         token = strsep(&buffer, " ");
         sscanf(token, "%lf", &val);
-        initial(matrix, size, count, line_number, val);
-        count++;
+        initial(mat, count, line_number, val);
     }
 }
 
-void insertMat(double *matrix, int size, int col, int row, double x) {
+void insertMat(Matrix *mat, int col, int row, double x) {
     //printf("inserting:%lf\n", x);
-    matrix[col + size * 2 * row] = x;
+    mat -> data[col + mat -> size * 2 * row] = x;
 }
 
-void initial(double *matrix, int size, int col, int row, double x) {
-    insertMat(matrix, size, col, row, x);
-    if (col == row) matrix[col + size * (1 + 2 * row)] = 1;
-    else matrix[col + size * (1 + 2 * row)] = 0;
+void initial(Matrix *mat, int col, int row, double x) {
+    insertMat(mat, col, row, x);
+    if (col == row) mat -> data[col + mat -> size * (1 + 2 * row)] = 1;
+    else mat -> data[col + mat -> size * (1 + 2 * row)] = 0;
 }
 
-double accessMat(double *matrix, int size, int col, int row) { //somthing wrong here
-    return matrix[col + size * 2 * row];
+double accessMat(Matrix *mat, int col, int row) { //somthing wrong here
+    return mat -> data[col + mat -> size * 2 * row];
 }
 
-void printMat(double *matrix, int size, int start) { //It would be interesting to come back to this. For some reason I can't use nested for loops. Printf is causing problems.
+void printMat(Matrix *mat, FILE *fp, int start) {
     int i,j;
-    for (j = 0; j < size; j++) {
-        for (i=start; i<size*2; i++) {
-            printf("%lf ",matrix[j*size*2 + i]);
+    for (j = 0; j < mat -> size; j++) {
+        for (i=start; i<mat -> size*2; i++) {
+            fprintf(fp,"%lf ", mat -> data[j*mat -> size*2 + i]);
         }
-        printf("\n");
+        fprintf(fp,"\n");
     }
 }
 
-void swapRow(double *matrix, int size, int ra, int rb) {
+void swapRow(Matrix *mat, int ra, int rb) {
     int i;
     double a,b;
-    for (i = 0; i < size * 2; i++) {
-        a = accessMat(matrix, size, i, ra);
-        b = accessMat(matrix, size, i, rb);
+    for (i = 0; i < mat -> size * 2; i++) {
+        a = accessMat(mat, i, ra);
+        b = accessMat(mat, i, rb);
         //printf("a:%lf b:%lf\n",a,b);
-        insertMat(matrix, size, i, rb, a);
-        insertMat(matrix, size, i, ra, b);
+        insertMat(mat, i, rb, a);
+        insertMat(mat, i, ra, b);
     }
 }
 
-int pivMax(double *matrix, int size, int k) {
+int pivMax(Matrix *mat, int k) {
     int i_max = k;
     int i_max_val = 0;
     int i,tmp;
-    for (i = k; i < size; i++) {
-        tmp = accessMat(matrix, size, k, i);
+    for (i = k; i < mat -> size; i++) {
+        tmp = accessMat(mat, k, i);
         //printf("tmp: %d\n",tmp);
         if (abs(tmp) > i_max_val) {
             i_max_val = abs(tmp);
@@ -110,62 +139,61 @@ int pivMax(double *matrix, int size, int k) {
     return i_max;
 }
 
-int gaussJordan(double *matrix, int size) {
+int gaussJordan(Matrix *mat) {
     int i, j,k, i_max;
     double tmp;
-    for (k = 0; k < size; k++) {
-        i_max = pivMax(matrix, size, k);
+    for (k = 0; k < mat -> size; k++) {
+        i_max = pivMax(mat, k);
         //printf("i_max: %d k: %d\n",i_max,k);
-        if(accessMat(matrix, size, k, i_max) == 0) return 1;
-        if(k != i_max) swapRow(matrix, size, k, i_max);
-        
-        for (j = k + 1; j < size; j++) {
-            for (i = k + 1; i < size * 2; i++) {
-                tmp = accessMat(matrix, size, i, j) - accessMat(matrix, size, i, k) * (accessMat(matrix, size, k, j) / accessMat(matrix, size, k, k));
-                insertMat(matrix,size,i,j,tmp);
+        assert(accessMat(mat, k, i_max) != 0);
+        if(k != i_max) swapRow(mat, k, i_max);
+        for (j = k + 1; j < mat -> size; j++) {
+            for (i = k + 1; i < mat -> size * 2; i++) {
+                tmp = accessMat(mat, i, j) - accessMat(mat, i, k) * (accessMat(mat, k, j) / accessMat(mat, k, k));
+                insertMat(mat, i, j, tmp);
             }
-            insertMat(matrix,size,k,j,0);
+            insertMat(mat,k,j,0);
         }
     }
-    backSub(matrix,size);
-    inverse(matrix,size);
+    backSub(mat);
+    inverse(mat);
     return 0;
 }
 
-void backSub(double *matrix, int size) {
+void backSub(Matrix* mat) {
     int i, j,k;
     double tmp;
-    for (k = size -1 ; k >= 0; k--) {
+    for (k = mat -> size -1 ; k >= 0; k--) {
         for (j = k - 1; j >= 0; j--) {
-            for (i = k + 1; i < size * 2; i++) {
-                tmp = accessMat(matrix, size, i, j) - accessMat(matrix, size, i, k) * (accessMat(matrix, size, k, j) / accessMat(matrix, size, k, k));
-                insertMat(matrix,size,i,j,tmp);
+            for (i = k + 1; i < mat -> size * 2; i++) {
+                tmp = accessMat(mat, i, j) - accessMat(mat, i, k) * (accessMat(mat, k, j) / accessMat(mat, k, k));
+                insertMat(mat,i,j,tmp);
             }
-            insertMat(matrix,size,k,j,0);
+            insertMat(mat,k,j,0);
         }
     }
 }
 
-void inverse(double *matrix, int size) {
+void inverse(Matrix *mat) {
     int k,i;
     double current, inv;
     
-    for (k = 0; k< size; k++) {
-        current = accessMat(matrix,size,k,k);
+    for (k = 0; k< mat -> size; k++) {
+        current = accessMat(mat, k, k);
         inv = 1/current;
-        for (i = k; i < size * 2; i++) {
+        for (i = k; i < mat -> size * 2; i++) {
             //printf("accessing: (%d,%d)\n",i,k);
-            current = accessMat(matrix,size,i,k);
-            insertMat(matrix,size,i,k,current * inv);
+            current = accessMat(mat,i,k);
+            insertMat(mat,i,k,current * inv);
         }
     }
 }
 
 int main() {
-    int size = 0;
-    double *m = readMatrix("input.txt", &size);
-    if(gaussJordan(m,size) == 1) printf("error\n");;
-    printMat(m,size,size);
-    free(m);
+    Matrix *mat = readMatrix("input.txt");
+    if(gaussJordan(mat) == 1) printf("error\n");;
+    //printMat(mat,mat -> size);
+    writeMatrix(mat);
+    destroyMat(mat);
     return 0;
 }
